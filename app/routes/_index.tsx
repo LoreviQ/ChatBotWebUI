468f5babfa621fba3d1c4eda28438b698a93e0d1
LoreviQ <lorevi@protonmail.com>
2024-10-17 00:33:22 +0100

    Change colours

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,12 +18,12 @@ export default function Index() {
                     <textarea
                         id="chat"
                         rows={4}
-                        className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="block p-2.5 w-full text-sm rounded-lg border text-gray-900 bg-white border-pink-600 dark:bg-zinc-900 dark:placeholder-gray-400 dark:text-white"
                         placeholder="Send a message to Ophelia!"
                     ></textarea>
                     <button
                         type="submit"
-                        className="inline-flex justify-center ps-4 p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+                        className="inline-flex justify-center ps-4 p-2 text-pink-600 rounded-full cursor-pointer"
                     >
                         <svg
                             className="w-5 h-5 rotate-90 rtl:-rotate-90"
