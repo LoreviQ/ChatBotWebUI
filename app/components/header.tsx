5df08a25c69bba561dc3f4414dd9c70c0e9928dc
LoreviQ <lorevi@protonmail.com>
2024-10-18 23:31:38 +0100

    Added header style

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
-        <div className="absolute top-0 left-0 w-full h-14 bg-blue-500">
-            <p>Substitute header</p>
-        </div>
+        <div
+            className="
+                absolute top-0 left-0 w-full h-20 
+                backdrop-blur-sm backdrop-saturate-200 backdrop-contrast-150 bg-bg-dark/50 
+                border-double border-b-4 border-primary-dark
+            "
+        ></div>
     );
 }
