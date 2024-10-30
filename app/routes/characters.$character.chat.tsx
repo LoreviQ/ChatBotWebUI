604946072221af881f3b3470141d2a59573ae7c0
LoreviQ <lorevi@protonmail.com>
2024-10-30 01:15:22 +0000

    chat now takes you to most recent chat

--- app/routes/characters.$character.chat.tsx
+++ app/routes/characters.$character.chat.tsx
@@ -1,8 +1,13 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/react";
+import { api, endpoints } from "../utils/api";
 
-export async function loader({ request }: LoaderFunctionArgs) {
+export async function loader({ params, request }: LoaderFunctionArgs) {
     const url = new URL(request.url);
     const baseUrl = url.pathname.endsWith("/") ? url.pathname.slice(0, -1) : url.pathname;
-    return redirect(`${baseUrl}/1`);
+    const query = `username=Oliver&char_path=${params.character!}&limit=1`;
+    const response = await api().get(endpoints.threads(query));
+    const data = await response.data;
+    const thread_id = data[0].id;
+    return redirect(`${baseUrl}/${thread_id}`);
 }
