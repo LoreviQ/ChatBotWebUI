064580a548b19ce4a497c9c3bf08f7857565c707
LoreviQ <lorevi@protonmail.com>
2024-11-09 14:52:35 +0000

    Added border to right of sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -9,12 +9,10 @@ interface SidebarProps {
 }
 export function Sidebar({ userPrefs, loggedIn }: SidebarProps) {
     return (
-        <div className="flex w-full items-end justify-end py-4 space-y-4">
+        <div className="flex h-screen w-full justify-end py-4 space-y-4 border-r border-text-muted-dark">
             <div className="w-80 flex flex-col">
                 <Link to={"/"} className="text-5xl font-ophelia font-outline">
-                    <button className="" type="button">
-                        Echoes AI
-                    </button>
+                    Echoes AI
                 </Link>
             </div>
         </div>
