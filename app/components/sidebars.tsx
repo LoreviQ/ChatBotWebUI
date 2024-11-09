c9a914a07ef6e417d13db4ef43606ca58505b8ea
LoreviQ <lorevi@protonmail.com>
2024-11-09 17:51:12 +0000

    Added login status to feed

--- app/components/sidebars.tsx
+++ app/components/sidebars.tsx
@@ -1,8 +1,8 @@
-import { Link } from "@remix-run/react";
+import { useFetcher, Link } from "@remix-run/react";
 
 import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faGhost, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faGhost, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import type { Cookie } from "../utils/cookies";
 
@@ -12,14 +12,25 @@ interface SidebarProps {
     loggedIn: boolean;
 }
 export function Sidebar({ userPrefs, loggedIn }: SidebarProps) {
+    const fetcher = useFetcher();
     return (
         <div className="flex h-screen w-full justify-end py-4 border-r border-text-muted-dark">
-            <div className="w-80 flex flex-col space-y-4">
-                <Link to={"/"} className="text-5xl font-ophelia font-outline">
-                    Echoes AI
-                </Link>
-                <SidebarLink to={"/"} text="Home" icon={faHouse} />
-                <SidebarLink to={"/characters"} text="Characters" icon={faGhost} />
+            <div className="w-80 flex flex-col justify-between">
+                <div className="flex flex-col space-y-4">
+                    <Link to={"/"} className="text-5xl font-ophelia font-outline">
+                        Echoes AI
+                    </Link>
+                    <SidebarLink to={"/"} text="Home" icon={faHouse} />
+                    <SidebarLink to={"/characters"} text="Characters" icon={faGhost} />
+                </div>
+
+                {loggedIn ? (
+                    <fetcher.Form method="post" action="/logout">
+                        <SidebarLink to={""} text="Logout" icon={faUser} />
+                    </fetcher.Form>
+                ) : (
+                    <SidebarLink to={"/login"} text="Login" icon={faUser} />
+                )}
             </div>
         </div>
     );
@@ -31,16 +42,18 @@ interface SidebarLinkProps {
     icon: IconDefinition;
 }
 function SidebarLink({ to, text, icon }: SidebarLinkProps) {
-    return (
-        <Link to={to} className="text-2xl ">
-            <div className="inline-flex items-center pe-4 py-2 hover:bg-hover-dark rounded-full">
-                <div className="flex w-14 justify-center">
-                    <FontAwesomeIcon icon={icon} />
-                </div>
-                <p>{text}</p>
+    const content = (
+        <div className="text-2xl inline-flex items-center pe-4 py-2 hover:bg-hover-dark rounded-full">
+            <div className="flex w-14 justify-center">
+                <FontAwesomeIcon icon={icon} />
             </div>
-        </Link>
+            <p>{text}</p>
+        </div>
     );
+    if (to) {
+        return <Link to={to}>{content}</Link>;
+    }
+    return content;
 }
 
 export function SearchBar({ userPrefs, loggedIn }: SidebarProps) {
